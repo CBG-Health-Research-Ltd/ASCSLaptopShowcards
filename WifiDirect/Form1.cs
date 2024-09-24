03f5518f0fda69dd0cb262aa0f2154db4cb7e0e5
James Larry Gaines II <jlgaines2006@gmail.com>
2024-09-24 12:11:22 +0800

    Add closing of first instance

---
+++ WifiDirect/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WifiDirectHost
+{
+    public partial class frmClosePrevInstance : Form
+    {
+        public frmClosePrevInstance()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
